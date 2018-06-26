0b632486ee9eeaab2f68bb3c4186f2ce6fe84ec9
odoren <oliver.doren@gmail.com>
2018-06-26 16:30:10 -0400

    DRY

--- js/scripts.js
+++ js/scripts.js
@@ -12,19 +12,19 @@ function lightSwitch() {
 	var style = getComputedStyle(document.body);
 	var state = style.getPropertyValue("--state");
 	if (state == 1) {
-		document.body.style.setProperty("--text-color", "#fff");
-		document.body.style.setProperty("--link-text-color", "#000");
-		document.body.style.setProperty("--background-color", "#696969");
-		document.body.style.setProperty("--primary-color", "cyan");
-		document.body.style.setProperty("--secondary-color", "magenta");
-		document.body.style.setProperty("--state", 2);
+		style.setProperty("--text-color", "#fff");
+		style.setProperty("--link-text-color", "#000");
+		style.setProperty("--background-color", "#696969");
+		style.setProperty("--primary-color", "cyan");
+		style.setProperty("--secondary-color", "magenta");
+		style.setProperty("--state", 2);
 	}
 	else {
-		document.body.style.setProperty("--text-color", "#000");
-		document.body.style.setProperty("--link-text-color", "#fff");
-		document.body.style.setProperty("--background-color", "#f4f4f4");
-		document.body.style.setProperty("--primary-color", "blue");
-		document.body.style.setProperty("--secondary-color", "red");
-		document.body.style.setProperty("--state", 1);
+		style.setProperty("--text-color", "#000");
+		style.setProperty("--link-text-color", "#fff");
+		style.setProperty("--background-color", "#f4f4f4");
+		style.setProperty("--primary-color", "blue");
+		style.setProperty("--secondary-color", "red");
+		style.setProperty("--state", 1);
 	}
 }

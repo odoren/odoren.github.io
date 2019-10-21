a0413dd7b2658b2b45e514aea7690ec7dcb6e232
odoren <oliver.doren@gmail.com>
2019-10-21 13:04:49 -0400

    Refactor JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
-/** Writes "last modified" data to footer */
-function lastModifiedFooter() {
-	return document.write("last modified: " + new Date(document.lastModified).toLocaleDateString());
+window.onload=function() {
+	/** Load "last modified" footer */
+	insertLastModifiedFooter();
+
+	/** Load contact link tracking */
+	const queryResult = document.getElementsByClassName("contact-link");
+	Array.from(queryResult).forEach(function(element) {
+		element.addEventListener("click", analytics.track.bind(this, "Contact Clicked", {
+			contactType: element.id
+		}))
+	});
+
+	/** Load light switch button tracking */
+	const lightSwitch = document.getElementById("light-switch");
+	lightSwitch.addEventListener("click", switchLight);
+	lightSwitch.addEventListener("click", analytics.track.bind(this, "Light Switched"));
 }
 
 /** 
@@ -8,16 +21,15 @@ function lastModifiedFooter() {
  * Default state is "on" (root CSS var --state is equal to 1)
  * Clicking the switch changes the theme colors and state
  */
-function lightSwitch() {
-	var style = getComputedStyle(document.body);
-	var state = style.getPropertyValue("--state");
-	if (state == 1) {
+function switchLight() {
+	let state = getComputedStyle(document.body).getPropertyValue("--state");
+	if (state == 0) {
 		document.body.style.setProperty("--text-color", "#fff");
 		document.body.style.setProperty("--link-text-color", "#000");
 		document.body.style.setProperty("--background-color", "#696969");
 		document.body.style.setProperty("--primary-color", "cyan");
 		document.body.style.setProperty("--secondary-color", "magenta");
-		document.body.style.setProperty("--state", 2);
+		document.body.style.setProperty("--state", 1);
 	}
 	else {
 		document.body.style.setProperty("--text-color", "#000");
@@ -25,6 +37,15 @@ function lightSwitch() {
 		document.body.style.setProperty("--background-color", "#f4f4f4");
 		document.body.style.setProperty("--primary-color", "blue");
 		document.body.style.setProperty("--secondary-color", "red");
-		document.body.style.setProperty("--state", 1);
+		document.body.style.setProperty("--state", 0);
 	}
+}
+
+/** Writes "last modified" data to footer */
+function insertLastModifiedFooter() {
+	var footer = document.getElementById("last-modified-footer");
+	var lastModifiedFooter = document.createElement("p");
+	var text = document.createTextNode("last modified: " + new Date(document.lastModified).toLocaleDateString());
+	lastModifiedFooter.append(text);
+	footer.append(lastModifiedFooter);
 }
